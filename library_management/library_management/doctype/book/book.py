647d83cf4ae38e89377d0404ead19739aef32aae
kcashique <ashiquekc97@gmail.com>
2025-04-14 15:34:18 +0530

    book :  added a function to validate book stock.

--- library_management/library_management/doctype/book/book.py
+++ library_management/library_management/doctype/book/book.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2025, kcashique and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Book(Document):
 	pass
+
+
+def validate_book(doc, method):
+	if doc.stock < 0:
+		frappe.throw("No Stock Avialbele Now")
